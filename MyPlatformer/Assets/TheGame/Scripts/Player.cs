6fa6df205383917bfc1d05ef124ff10a84636625
nwiedel <164350832+nwiedel@users.noreply.github.com>
2024-05-24 23:51:26 +0200

    3.18. Input

---
+++ MyPlatformer/Assets/TheGame/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Steuerung der Spielfigur
+/// </summary>
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// Laufgeschwindigkeit des Players
+    /// </summary>
+    public float speed = 0.05f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += Input.GetAxis("Horizontal") * speed * transform.forward;
+    }
+}
