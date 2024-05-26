e8d8780ef47b91af7410ca99a5b50319c75af441
NicolasWiedel <nicolaswiedel@googlemail.com>
2024-05-26 21:37:29 +0200

    3.20.1 Drehung

--- MyPlatformer/Assets/TheGame/Scripts/Player.cs
+++ MyPlatformer/Assets/TheGame/Scripts/Player.cs
@@ -12,9 +12,21 @@ public class Player : MonoBehaviour
     /// </summary>
     public float speed = 0.05f;
 
+    /// <summary>
+    /// Das grafische Modell, u.a. für die Drehung in Laufrichtung
+    /// </summary>
+    public GameObject model;
+
     // Update is called once per frame
     private void Update()
     {
+        // Bewegung
         transform.position += Input.GetAxis("Horizontal") * speed * transform.forward;
+
+        // Rotation
+        if (Input.GetAxis("Horizontal") > 0)
+            model.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        else if(Input.GetAxis("Horizontal") < 0)
+            model.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
     }
 }
