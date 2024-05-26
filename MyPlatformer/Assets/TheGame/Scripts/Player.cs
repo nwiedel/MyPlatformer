d3e76514a8c5010f6b9169907ece44296238cfab
nwiedel <164350832+nwiedel@users.noreply.github.com>
2024-05-26 22:45:45 +0200

    3.20.2 Drehung des Spielers

--- MyPlatformer/Assets/TheGame/Scripts/Player.cs
+++ MyPlatformer/Assets/TheGame/Scripts/Player.cs
@@ -8,25 +8,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     /// <summary>
-    /// Laufgeschwindigkeit des Players
+    /// Laufgeschwindigkeit des Players.
     /// </summary>
     public float speed = 0.05f;
 
     /// <summary>
-    /// Das grafische Modell, u.a. für die Drehung in Laufrichtung
+    /// Das grafische Modell, u.a. für die Drehung in Laufrichtung.
     /// </summary>
     public GameObject model;
+
+    /// <summary>
+    /// Der Winkel, zu dem sich die Figur um die eigene Achse(=Y)
+    /// drehen soll.
+    /// </summary>
+    private float towardsY = 0f;
 
     // Update is called once per frame
     private void Update()
     {
+        // Speichert den Input
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         // Bewegung
-        transform.position += Input.GetAxis("Horizontal") * speed * transform.forward;
+        transform.position += horizontalInput * speed * transform.forward;
 
         // Rotation
-        if (Input.GetAxis("Horizontal") > 0)
-            model.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        else if(Input.GetAxis("Horizontal") < 0)
-            model.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        if (horizontalInput > 0) // nach rechts gehen
+            towardsY = 0f;
+        else if (horizontalInput < 0) // nach links gehen
+            towardsY = -180f; // - bedeutert, dass er sich in die Kamera dreht!
+        // Sanfter Übergang der Drehbewegung
+        model.transform.rotation = Quaternion.Lerp(
+            model.transform.rotation,
+            Quaternion.Euler(0f, towardsY, 0f),
+            Time.deltaTime * 10f);
     }
 }
