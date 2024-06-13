24e8ca021e7c0255944f2572b28cb5ad107cf215
nwiedel <nwiedel@alte-hof-apotheke.com>
2024-06-13 15:14:51 +0200

    6.41.1. Wechseln der Scene

---
+++ MyPlatformer/Assets/TheGame/Scripts/GoToScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToScene : MonoBehaviour
+{
+    /// <summary>
+    /// Name der Scene, die geladen wird, wenn die Figur
+    /// den Trigger auslöst.
+    /// </summary>
+    public string scene = "";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LevelManager lm = FindObjectOfType<LevelManager>();
+        if (lm != null)
+        {
+            lm.LoadScene(scene);
+        }
+    }
+}
--- MyPlatformer/Assets/TheGame/Scripts/LevelManager.cs
+++ MyPlatformer/Assets/TheGame/Scripts/LevelManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     private void Awake()
     {
         SaveGameData.current = SaveGameData.Load();
+    }
+
+    public void LoadScene(string name)
+    {
+        Debug.Log("Lade jetzt Scene: " + name);
+        SceneManager.LoadScene(name, LoadSceneMode.Additive);
     }
 
     private void Update()
